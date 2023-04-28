dd4d28c3df0b6db4237f6f734da267be6472d145
Jiachun Xiang <max997123@gmail.com>
2023-04-28 13:59:30 -0400

    revise functions

--- modules/bookCollection.js
+++ modules/bookCollection.js
@@ -2,25 +2,30 @@ import { Book } from "./book.js";
 
 // Function to display all books saved in the collection
 export const displayBooks = (myBookCollection) => {
-  const bookListElement = document.getElementById("bookList");
-  bookListElement.innerHTML = "";
-  myBookCollection.bookList.forEach((book) => {
-    const bookDiv = document.createElement("div");
-    bookDiv.innerHTML = `<h2>${book.title}</h2><p>${book.author}</p><button class="removeButton" data-title="${book.title}">Remove</button>`;
+  const bookListElement = document.getElementById('bookList');
+  bookListElement.innerHTML = '';
+  this.bookList.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('viewBook');
+    bookDiv.innerHTML = `<span>${book.title} by ${book.author}</span><button class="removeButton" data-title="${book.title}">Remove</button>`;
     bookListElement.appendChild(bookDiv);
   });
-};
+}
+
 
 // Function to add a new book to the collection
 export const addBook = (myBookCollection, title, author) => {
-  myBookCollection.addBook(title, author);
-  displayBooks(myBookCollection);
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-};
+  const book = { title, author };
+  this.bookList.push(book);
+  localStorage.setItem('bookCollection', JSON.stringify(this.bookList));
+  this.displayBooks();
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+}
 
 // Function to remove a book from the collection
 export const removeBook = (myBookCollection, title) => {
-  myBookCollection.removeBook(title);
-  displayBooks(myBookCollection);
+  this.bookList = this.bookList.filter((book) => book.title !== title);
+  localStorage.setItem('bookCollection', JSON.stringify(this.bookList));
+  this.displayBooks();
 };
